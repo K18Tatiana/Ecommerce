ea678758a3ca445b813320e5a72040dcd86782ed
K18Tatiana <tatianavegac2003@gmail.com>
2023-02-03 01:17:38 -0500

    Ecommerce

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,5 @@
 import { Row, Col, Button, Card } from "react-bootstrap"
 import { useNavigate } from "react-router-dom"
-import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { addToCartThunk } from "../store/slices/cart.slice"
 
@@ -29,41 +28,34 @@ const ProductCard = ( {product} ) => {
         <Col>
             <Card className="product-card">
                 <div className="img-wrapper">
-                    <a href="#page-top">
-                        <Card.Img 
-                        className="product-img"
-                        variant="top"
-                        src= {product?.productImgs?.[1]}
-                        onClick={ () => navigate(`/product/${product.id}`) }
-                        />
-                        <Card.Img 
-                        className="product-img cover-img"
-                        variant="top"
-                        src= {product?.productImgs?.[0]}
-                        onClick={ () => navigate(`/product/${product.id}`) }
-                        />
-                    </a>
+                    <Card.Img 
+                    className="product-img"
+                    variant="top"
+                    src= {product?.productImgs?.[1]}
+                    onClick={ () => navigate(`/product/${product.id}`) }
+                    />
+                    <Card.Img 
+                    className="product-img cover-img"
+                    variant="top"
+                    src= {product?.productImgs?.[0]}
+                    onClick={ () => navigate(`/product/${product.id}`) }
+                    />
                 </div>
-                
                 <hr />
                 <Card.Body 
                 style={ {display: 'flex', flexDirection: 'column', justifyContent: 'space-around'} }
                 >
-                    <a href="#page-top" style={ {textDecoration: 'none'} }>
-                        <Card.Title 
-                        className="text-primary" 
-                        onClick={ () => navigate(`/product/${product.id}`) }
-                        style={ {cursor: 'pointer'} }
-                        >
-                            {product?.title}
-                        </Card.Title>
-                    </a>
+                    <Card.Title 
+                    className="text-primary" 
+                    onClick={ () => navigate(`/product/${product.id}`) }
+                    style={ {cursor: 'pointer'} }
+                    >
+                        {product?.title}
+                    </Card.Title>
                     <Row>
                         <Col onClick={ () => navigate(`/product/${product.id}`) } style={ {cursor: 'pointer'} }>
-                            <a href="#page-top" style={ {textDecoration: 'none'} }>
-                                <Card.Subtitle className="mb-2 text-muted">Price</Card.Subtitle>
-                                <Card.Text>$ {product?.price}</Card.Text>
-                            </a>
+                            <Card.Subtitle className="mb-2 text-muted">Price</Card.Subtitle>
+                            <Card.Text>$ {product?.price}</Card.Text>
                         </Col>
                         <Col>
                             <Button style={ {fontSize: 20} } onClick={ () => addToCart(product.id) }>
